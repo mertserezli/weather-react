0583d01222126a30626384aba87c28ef33b395e5
Mert Serezli <mertserezli@gmail.com>
2021-01-17 20:19:37 +0300

    add apikey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+require('dotenv').config();
+
+const api = {
+    key: process.env.REACT_APP_WEATHER_API_KEY,
+    base: "https://api.openweathermap.org/data/2.5/weather?q="
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
