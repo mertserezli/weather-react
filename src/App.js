4e5892e9c83fa6e25602cc262b736e8e569fd93d
Mert Serezli <mertserezli@gmail.com>
2021-01-18 13:11:28 +0300

    show values from api

--- src/App.js
+++ src/App.js
@@ -40,14 +40,14 @@ function App() {
                 {(typeof weather.main != "undefined") ? (
                 <div>
                     <div className="location-box">
-                        <div className="location">City Name</div>
+                        <div className="location">{weather.name}, {weather.sys.country}</div>
                         <div className="date">{new Date().toDateString()}</div>
                     </div>
                     <div className="weather-box">
                         <div className="temp">
-                            15°c
+                            {Math.round(weather.main.temp)}°c
                         </div>
-                        <div className="weather">Sunny</div>
+                        <div className="weather">{weather.weather[0].main}</div>
                     </div>
                 </div>
                 ) : ('')}
